16b70ee4a21fb22af3870fdf83bca79d627b4158
AwlMax <maxshilo159@gmail.com>
2022-06-22 15:16:55 +0300

    Update initDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -29,9 +29,9 @@ CREATE TABLE meals
 (
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     user_id          INTEGER                           NOT NULL,
-    datetime         TIMESTAMP                         NOT NULL,
+    date_time        TIMESTAMP                         NOT NULL,
     description      VARCHAR                           NOT NULL,
     calories         INTEGER                           NOT NULL,
-    CONSTRAINT meals_idx UNIQUE  (user_id, datetime),
+    CONSTRAINT meals_user_date_time_unique_idx UNIQUE  (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
